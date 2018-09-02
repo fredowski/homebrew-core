c41e0d7538f016b7dee034fc284fe1a5a18e60a2
Friedrich Beckmann <friedrich.beckmann@gmx.de>
2018-09-02 10:35:18 +0200

    ant - fixed download link
    The download link did not work.
    Closes: #31692

--- Formula/ant.rb
+++ Formula/ant.rb
@@ -1,7 +1,7 @@
 class Ant < Formula
   desc "Java build tool"
   homepage "https://ant.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=ant/binaries/apache-ant-1.10.5-bin.tar.xz"
+  url "https://www.apache.org/dyn/closer.cgi?filename=ant/binaries/apache-ant-1.10.5-bin.tar.xz&action=download", :using => :curl
   sha256 "cebb705dbbe26a41d359b8be08ec066caba4e8686670070ce44bbf2b57ae113f"
   head "https://git-wip-us.apache.org/repos/asf/ant.git"
 
@@ -15,12 +15,12 @@ class Ant < Formula
   depends_on :java => "1.8+"
 
   resource "ivy" do
-    url "https://www.apache.org/dyn/closer.cgi?path=ant/ivy/2.4.0/apache-ivy-2.4.0-bin.tar.gz"
+    url "https://www.apache.org/dyn/closer.cgi?filename=ant/ivy/2.4.0/apache-ivy-2.4.0-bin.tar.gz&action=download", :using => :curl
     sha256 "7a3d13a80b69d71608191463dfc2a74fff8ef638ce0208e70d54d28ba9785ee9"
   end
 
   resource "bcel" do
-    url "https://www.apache.org/dyn/closer.cgi?path=commons/bcel/binaries/bcel-6.2-bin.tar.gz"
+    url "https://www.apache.org/dyn/closer.cgi?filename=commons/bcel/binaries/bcel-6.2-bin.tar.gz&action=download", :using => :curl
     sha256 "e5963ed50ef1f243f852a27efaf898c050a3b39721d147ccda8418c0b7255955"
   end
 
